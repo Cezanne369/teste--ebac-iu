c17cf1f2c1c622e12f8fd50a6b11e1b079791433
Jean Paul <saopaulojean74@gmail.com>
2025-02-15 21:40:18 -0300

    alterações de after e before

--- cypress/e2e/3-loja-ebac/login.cy.js
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -1,16 +1,40 @@
 ///<reference types="cypress"/>
 
-const { it } = require("mocha")
-
 describe('funcionalidade: Login', () => {
+    
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    });
+
+    afterEach(() => {
+        cy.screenshot()
+    });
 
     it('Deve fazer login com sucesso', () => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    
         cy.get('#username').type('saopaulojean74@gmail.com')
         cy.get('#password'). type ('teste 123') 
         cy.get('.woocommerce-form > .button').click()
-
         cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, saopaulojean74')
 
     })
+
+    it('Deve exibir uma mensagem de erro ao inserir usuário inválido', () => {
+
+       
+        cy.get('#username').type('saopaulon74@gmail.com')
+        cy.get('#password'). type ('teste 123') 
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-error > li').should('exist')
+    });
+
+    it('eve exibir uma mensagem de erro ao inserir senha inválido', () => {
+ 
+        cy.get('#username').type('saopaulojean74@gmail.com')
+        cy.get('#password'). type ('teste123') 
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-error > li').should('contain','A senha fornecida para o e-mail saopaulojean74@gmail.com está incorreta')
+
+    });
+
 })
