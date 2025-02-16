7d3f65402e1a63d367d238e85de00910eb5f35d3
Jean Paul <saopaulojean74@gmail.com>
2025-02-16 14:43:44 -0300

    Novo arquivo de produto

---
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -0,0 +1,20 @@
+///<reference types="cypress"/>
+
+describe('Funcionalidade: produtos', () => {
+    
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+    });
+
+    it('Deve selecionar um produto da lista', () => {
+        cy.get('.products > .row')
+        //.first()
+        //.last()
+        //.eq(1)
+        .contains('Apollo Running Short')
+        .click()
+        cy.get('.tbay-woocommerce-breadcrumb > :nth-child(6)').should('contain', 'Apollo Running Short')
+    });
+
+
+});
