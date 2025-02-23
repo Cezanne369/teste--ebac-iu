24e09fc10e679ccd1e7f29cd4ee1df8f7d5e82d7
Jean Paul <saopaulojean74@gmail.com>
2025-02-23 00:26:48 -0300

    alteração adicionando produtopage

--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -1,19 +1,48 @@
 ///<reference types="cypress"/>
 
+import produtosPage from "../../support/page-objects/produtos-page";
+
+produtosPage
+
 describe('Funcionalidade: produtos', () => {
     
     beforeEach(() => {
-        cy.visit('produtos')
+        produtosPage.visitarUrl()
     });
 
     it('Deve selecionar um produto da lista', () => {
-        cy.get('.products > .row')
-        //.first()
-        //.last()
-        //.eq(1)
-        .contains('Apollo Running Short')
-        .click()
-        cy.get('.tbay-woocommerce-breadcrumb > :nth-child(6)').should('contain', 'Apollo Running Short')
+        produtosPage.buscarProdutoLista('Aero Daily Fitness Tee')
+        cy.get('.tbay-woocommerce-breadcrumb > :nth-child(6)').should('contain', 'Aero Daily Fitness Tee')
+    });
+
+    it('Deve selecionar um produto da lista', () => {
+        produtosPage.buscarProduto
+    });
+
+    it('Deve buscar um produto com sucesso', () => {
+        let produto = 'Ariel Roll Sleeve Sweatshirt'
+       produtosPage.buscarProduto(produto) 
+       cy.get('.product_title').should('contain', produto)
+    });
+
+    it('Deve visitar a página do produto', () => {
+        produtosPage.visitarProduto('Zoltan Gym Tee')
+        cy.get('.product_title').should('contain', 'Zoltan Gym Tee')
+    });
+
+    it.only('Deve adicionar produto ao carrinho buscando da massa de dados', () => {
+        cy.fixture('produtos').then(dados => { 
+            produtosPage.buscarProduto(dados[0].nomeProduto)
+            produtosPage.addProdutoCarrinho(
+                dados[0].tamanho, 
+                dados[0].cor, 
+                dados[0].quantidade)
+            cy.get('.woocommerce-message').should('contain', dados[0].nomeProduto)
+
+        })
+
+        
+        
     });
 
 
